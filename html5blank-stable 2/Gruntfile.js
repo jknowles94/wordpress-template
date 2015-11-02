93926d4ae3d30fe924646c9b1dfb6310ab248791
Jake Knowles <jakeyknowles@hotmail.co.uk>
2015-11-02 12:59:49 +0000

    Wordpress template

---
+++ html5blank-stable 2/Gruntfile.js
@@ -0,0 +1,178 @@
+// To check for new package updates see https://www.npmjs.com/package/npm-check-updates
+module.exports = function (grunt) {
+
+    // Configurable options
+    var config = {
+        sassPath:       'assets/sass',
+        imagesPath:     'assets/images',
+        jsPath:         'assets/scripts',
+        sourcemap:      false
+    };
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Project options
+        config: config,
+
+        // Compass options used to compile SASS into CSS
+        compass: {
+            dev: {
+                options: {
+                    sassDir: '<%= config.sassPath %>',
+                    cssDir: '../html5blank-stable 2/',
+                    environment: 'development',
+                    sourcemap: '<%= config.sourcemap %>'
+                }
+            },
+            live: {
+                options: {
+                    sassDir: '<%= config.sassPath %>',
+                    cssDir: '../html5blank-stable 2/',
+                    environment: 'production'
+                }
+            }
+        },
+
+        // Add vendor prefixed styles
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 version', 'last 4 Explorer versions'], 
+                map: '<%= config.sourcemap %>'
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    src: 'style.css',
+                    cwd: '../html5blank-stable 2/',
+                    dest: '../html5blank-stable 2/'
+                }]
+            }
+        },
+
+        // Watchs files for changes then compiles and reloads the browser
+        watch: {
+
+            html: {
+                files: ['templates/index.html','templates/styleguide.html','templates/template.html'],
+                options: {
+                    livereload: true
+                }
+            },
+
+            compass: {
+                files: ['<%= config.sassPath %>/{,*/}*.{scss,sass}'],
+                tasks: ['compass:dev', 'autoprefixer', 'notify:compass'],
+                options: {
+                    livereload: true
+                }
+            },
+
+            jshint: {
+                files: ['<%= config.jsPath %>/main.js'],
+                tasks: ['jshint']
+            }
+
+        },
+
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : 'style.css'
+                },
+                options: {
+                    proxy: 'http://my-site.local',
+                    watchTask: true
+                }
+            }
+        },
+
+        // Checks JS file for errors
+        jshint: {
+            all: ['<%= config.jsPath %>/main.js'],
+            options: {
+                '-W099': true, // Stops mixed tabs and spaces error
+            },
+        },
+
+        // Clean any pre-commit hooks in .git/hooks directory
+        clean: {
+            precommit: ['.git/hooks/pre-commit'],
+            pull: ['.git/hooks/post-merge']
+        },
+
+        shell: {
+            precommit: {
+                command: 'cp git-hooks/pre-commit .git/hooks/'
+            },
+            pull: {
+                command: 'cp git-hooks/post-merge .git/hooks/'
+            }
+        },
+
+        notify: {
+            compass: {
+              options: {
+                title: 'Wooden Spoon', 
+                message: 'Compass compiled',
+              }
+            }
+        },
+
+
+        // Reads the js files from the specified html file and generates the concat & uglify config, run with grunt buildJS
+        useminPrepare: {
+            html: 'templates/template.html'
+        },
+
+        // Conatenates files
+        concat: {
+            build: {
+                files: [ 
+                    { dest: 'assets/scripts/plugins.js',
+                    
+                    src: 
+                        [ 
+                             'bower_components/SimpleStateManager/dist/ssm.min.js',
+                             'bower_components/jquery-colorbox/jquery.colorbox-min.js',
+                             'bower_components/bootstrapValidator/dist/js/bootstrapValidator.min.js',
+                             'bower_components/respond/dest/respond.min.js',
+                        ] 
+                    } 
+                ]
+            }
+        },
+
+        // Minifies JS files
+        uglify: {
+            build: {
+                files: {
+                    '<%= config.jsPath %>/plugins.min.js': ['<%= config.jsPath %>/plugins.js'],
+                    '<%= config.jsPath %>/main.min.js': ['<%= config.jsPath %>/main.js'],
+                    '<%= config.jsPath %>/modernizr.min.js': ['<%= config.jsPath %>/modernizr.js']
+                }
+            }
+        },
+
+        // Minifies CSS files
+        cssmin: {
+            build: {
+                files: {
+                    'style.min.css': ['style.css']
+                }
+            }
+        }
+
+    });
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Default task(s)
+    grunt.registerTask('default', ['compass:dev', 'autoprefixer']);
+    grunt.registerTask('watchsync', ['browserSync', 'watch']);
+    grunt.registerTask('setup', ['clean:precommit','shell:precommit','clean:pull','shell:pull']);
+    grunt.registerTask('live', ['jshint', 'uglify', 'compass:live', 'autoprefixer', 'cssmin']);
+    grunt.registerTask('buildJS', ['useminPrepare','concat:generated']);
+};
